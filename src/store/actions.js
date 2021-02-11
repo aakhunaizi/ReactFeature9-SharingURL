8db83b8b2d739199de10481644147ab13e38173e
aakhunaizi <62260811+aakhunaizi@users.noreply.github.com>
2021-02-11 16:22:58 +0300

    Added CRUD from database

--- src/store/actions.js
+++ src/store/actions.js
@@ -3,10 +3,10 @@
 import axios from "axios";
 
 // ACTION TYPES
+export const CREATE_PRODUCT = "CREATE_PRODUCT";
 export const FETCH_PRODUCTS = "FETCH_PRODUCTS";
-export const ADD_PRODUCT = "ADD_PRODUCT";
-export const DELETE_PRODUCT = "DELETE_PRODUCT";
 export const UPDATE_PRODUCT = "UPDATE_PRODUCT";
+export const DELETE_PRODUCT = "DELETE_PRODUCT";
 
 // ACTIONS
 
@@ -24,17 +24,47 @@ export const fetchProducts = () => {
   };
 };
 
-export const createProduct = (newProduct) => ({
-  type: ADD_PRODUCT,
-  payload: { newProduct },
-});
+export const createProduct = (newProduct) => {
+  return async (dispatch) => {
+    try {
+      await axios.post(`http://localhost:8000/products/`, newProduct);
+      dispatch({
+        type: CREATE_PRODUCT,
+        payload: { newProduct },
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+};
 
-export const deleteProduct = (productId) => ({
-  type: DELETE_PRODUCT,
-  payload: { productId: productId },
-});
+export const deleteProduct = (productId) => {
+  return async (dispatch) => {
+    try {
+      await axios.delete(`http://localhost:8000/products/${productId}`);
+      dispatch({
+        type: DELETE_PRODUCT,
+        payload: { productId },
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+};
 
-export const updateProduct = (updatedProduct) => ({
-  type: UPDATE_PRODUCT,
-  payload: { updatedProduct: updatedProduct },
-});
+export const updateProduct = (updatedProduct) => {
+  return async (dispatch) => {
+    try {
+      await axios.put(
+        `http://localhost:8000/products/${updatedProduct.id}`,
+        updatedProduct
+      );
+      dispatch({
+        type: UPDATE_PRODUCT,
+        payload: { updatedProduct },
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+};
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,5 +1,5 @@
 import {
-  ADD_PRODUCT,
+  CREATE_PRODUCT,
   DELETE_PRODUCT,
   UPDATE_PRODUCT,
   FETCH_PRODUCTS,
@@ -21,7 +21,7 @@ const reducer = (state = initialState, action) => {
         loading: false,
       };
 
-    case ADD_PRODUCT:
+    case CREATE_PRODUCT:
       const { newProduct } = action.payload;
       newProduct.slug = slugify(newProduct.name, { lower: true });
       newProduct.id = state.products[state.products.length - 1].id + 1;
@@ -29,6 +29,7 @@ const reducer = (state = initialState, action) => {
         ...state,
         products: [...state.products, newProduct],
       };
+
     case DELETE_PRODUCT:
       return {
         ...state,
